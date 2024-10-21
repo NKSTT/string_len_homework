78be3d78dcf21e0cc93fcb633691ac7ae59037d9
NKSTT <asultamuratov110@gmail.com>
2024-10-21 21:32:17 +0800

    string len misol 7

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,21 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    
+    if len(s1)%2 != 0 and len(s2)%2 != 0 and len(s3)%2 == 0:
+        return s1, s2
+    elif len(s1)%2 != 0 and len(s2)%2 == 0 and len(s3)%2 != 0:
+        return s1, s3
+    elif len(s1)%2 == 0 and len(s2)%2 != 0 and len(s3)%2 != 0:
+        return s2, s3
+
+    if len(s1)%2 == 0 and len(s2)%2 == 0 and len(s3)%2 != 0:
+        return s3
+    elif len(s1)%2 == 0 and len(s2)%2 != 0 and len(s3)%2 == 0:
+        return s2
+    elif len(s1)%2 != 0 and len(s2)%2 == 0 and len(s3)%2 == 0:
+        return s1
+    else:
+        return s1, s2, s3
+    
+print (main('qwer','hkkk','qwertyui'))
